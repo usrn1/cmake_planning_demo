5eaf2f9278345b05442bae8a6eed9b3e8c0b19d0
usrn1 <l1229924712@163.com>
2024-04-06 17:16:02 +0800

    version 0.0.2

---
+++ src/planning_main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"process.h"
+
+using namespace std;
+
+int main(){
+    cout<<"planning start"<<endl;    
+    Process pro;
+    pro.planProcess();
+    cout<<"planning end"<<endl;
+
+    return 0;
+}
